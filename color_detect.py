c6c4242554d1d62efb3ec0c07afc1414fc30dad4
Jeffin Francis <francisjeffin07@gmail.com>
2019-10-02 14:20:08 +0530

    color detection added
    color detection added for red,green,blue

--- color_detect.py
+++ color_detect.py
@@ -1,29 +1,61 @@
-#!/usr/bin/env python
 import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
 
-def greenCircleDetect():
+def greenCircleDetect(image):
+	green = [([65,60,60], [80,255,255])]
+	for (lower, upper) in green:
+	# create NumPy arrays from the boundaries
+		lower = np.array(lower, dtype = "uint8")
+		upper = np.array(upper, dtype = "uint8")
+ 
+		# find the colors within the specified boundaries and apply
+		# the mask
+		mask = cv2.inRange(image, lower, upper)
+		output = cv2.bitwise_and(image, image, mask = mask)
+		return output
+
+def redCircleDetect(image):
+	red  = [([17, 15, 100], [50, 56, 200])]
+	for (lower, upper) in red:
+	# create NumPy arrays from the boundaries
+		lower = np.array(lower, dtype = "uint8")
+		upper = np.array(upper, dtype = "uint8")
+ 
+		# find the colors within the specified boundaries and apply
+		# the mask
+		mask = cv2.inRange(image, lower, upper)
+		output = cv2.bitwise_and(image, image, mask = mask)
+		return output
+
+def blueCircleDetect(image):
+	blue = [([60, 31, 4], [220, 88, 50])]
+	for (lower, upper) in blue:
+	# create NumPy arrays from the boundaries
+		lower = np.array(lower, dtype = "uint8")
+		upper = np.array(upper, dtype = "uint8")
+ 
+		# find the colors within the specified boundaries and apply
+		# the mask
+		mask = cv2.inRange(image, lower, upper)
+		output = cv2.bitwise_and(image, image, mask = mask)
+		return output
 
 
-def redCircleDetect():
-
-
-def blueCircleDetect():
 
 
 while True:
 	ret, frame = cap.read()
 
-	greenCircleDetect()
+	green_image = greenCircleDetect(frame)
 
-	redCircleDetect()
+	red_image = redCircleDetect(frame)
 
-	blueCircleDetect()
+	blue_image = blueCircleDetect(frame)
 
-
-	cv2.imshow('frame', frame)
+	cv2.imshow("images", np.hstack([frame, red_image, green_image, blue_image]))
+	# cv2.imshow('frame', frame)
 
 	cv2.waitKey(3)
 
